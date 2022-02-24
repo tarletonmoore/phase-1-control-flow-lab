431651b1e5ed29d71b313cd027d7f42c4beaed70
John Tarleton Moore <tarletonmoore@gmail.com>
2022-02-23 20:28:00 -0500

    commit tree d2ae145e84922c00a6840b0ce5507ac3eca9b12c

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue) {
   // Write your code here!
+  if (someValue <= 400) {
+    return `This one is on me!`
+  }
+  else if (someValue > 2500) {
+    return `No can do.`
+  }
+  else if (someValue > 2000) {
+    return `I will gladly take your thirty bucks.`
+  }
+}
+scuberGreetingForFeet(199)
+
+function ternaryCheckCity(city) {
+  // Write your code here!
+  return city === `NYC` ? `Ok, sounds good.` : `No go.`
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(feeling) {
   // Write your code here!
+  switch (feeling) {
+    case `generous`:
+      return `Thank you so much.`
+    case `not as generous`:
+      return `Thank you.`
+    default:
+      return `Bye.`
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+// what is a function parameter and what is its purpose, when do arguments get passed in?
+// do i need to invoke in the test driven environment?
